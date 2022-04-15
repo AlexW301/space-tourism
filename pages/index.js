62b2c6798f7c656dc8ea7cfe8cf5cf720d2f0fbe
Alex Waller <alex@pmlhomeloans.com>
2022-04-15 10:29:42 -0400

    update home page

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,25 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.scss'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
 
 export default function Home() {
   return (
-    <div className={styles.main}>
-      <h1>test</h1>
+    <div className={styles.container}>
+      <div className={styles.main}>
+        <section className={styles.callToActionSection}>
+          <h5>So, You want to travel to space</h5>
+          <h1>Space</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </section>
+        <section className={styles.buttonSection}>
+          <button>explore</button>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
