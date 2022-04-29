f55555fe1a342d9f825b115c7dfed582b076cf6f
Alex Waller <alex@pmlhomeloans.com>
2022-04-29 08:00:09 -0400

    update to technology page

--- pages/technology.js
+++ pages/technology.js
@@ -1,28 +1,29 @@
 /* eslint-disable @next/next/no-img-element */
 import styles from "../styles/Technology.module.scss";
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 import { gsap } from "gsap";
 
 const Technology = () => {
+  const { height, width } = useWindowDimensions();
   const [tab, setTab] = useState(0);
   const content = useRef(null);
   const picture = useRef(null);
-
   const technologyData = [
     {
       name: "Launch Vehicle",
       desc: "A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth's surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it's quite an awe-inspiring sight on the launch pad!",
-      image: "/assets/technology/image-launch-vehicle-portrait.jpg",
+      image: width > 1233 ? "/assets/technology/image-launch-vehicle-portrait.jpg" : "/assets/technology/image-launch-vehicle-landscape.jpg",
     },
     {
       name: "Spaceport",
       desc: "A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.",
-      image: "/assets/technology/image-spaceport-portrait.jpg",
+      image: width > 1233 ? "/assets/technology/image-spaceport-portrait.jpg" : "/assets/technology/image-spaceport-landscape.jpg",
     },
     {
       name: "Space Capsule",
       desc: "A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth's atmosphere without wings. Our capsule is where you'll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.",
-      image: "/assets/technology/image-space-capsule-portrait.jpg",
+      image: width > 1233 ? "/assets/technology/image-space-capsule-portrait.jpg" : "/assets/technology/image-space-capsule-landscape.jpg",
     }
   ];
 
